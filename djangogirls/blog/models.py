f710270de5be1c62168bac8606670609466ba70f
zooozoo <shz0309@gmail.com>
2017-09-25 14:33:51 +0900

    Post정의, migration생성, admin추가

--- djangogirls/blog/models.py
+++ djangogirls/blog/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
